2bbc56bbddcfafe8effb77077f709de35c1dd843
Anthony <eminent0225@gmail.com>
2020-06-21 21:09:27 +0800

    change while to i>0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 password = "a123456"
 i = 3
-while True:
+while i > 0:
 	code = input("請輸入密碼")
 	if code == password:
 		print("登入成功")
@@ -8,9 +8,6 @@ while True:
 	else :
 		i = i - 1
 		print("密碼錯誤還有", i,"次機會")
-		if i == 0:
-		    print("請過十分鐘後在回來")
-		    break
 
 
 
